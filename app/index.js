e396af9f86d202f64e9192c9fed3e933522b5189
cheporozo <da132413@gmail.com>
2024-01-30 20:34:41 -1100

    Segundo Commit
    desarrollo del frontend

--- app/index.js
+++ app/index.js
@@ -1,7 +1,22 @@
-import  express  from "express";
+import express from "express"
+//arreglazo para dirname lol 
+import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
 
 //server 
 const app = express();
-app.set("port",4000)
+app.set("port", 3000);
 app.listen(app.get("port"));
-console.log("servidor on puerto", app.get("port"))
+console.log("server activo on puerto",app.get("port"))
+
+//config
+app.use(express.static(__dirname + "/publicazo"))
+
+
+//routes 
+app.get("/",(req,res)=> res.sendFile(__dirname + "/paginitas/login.html"))
+app.get("/register",(req,res)=> res.sendFile(__dirname + "/paginitas/register.html"))
+
