e953579d530fd2a5f877b8d14151ea13853ce463
cheporozo <da132413@gmail.com>
2024-02-01 20:52:59 -1100

    cuarto commit

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,11 +1,41 @@
+import bcryptjs from "bcryptjs";
+
+const usuarios  = [{
+    user: "a",
+    email: "a@a.com",
+    password: "a"
+}]
+
+
 async function login(req,res){
 }
 
 async function register(req,res){
     console.log(req,body)
+    const user = req.body.user; 
+    const password = req.body.password; 
+    const email = req.body.email; 
+    if(!user || !password || !email){
+        return res.status(400).send({status:"Error",message:"Los campos están incompletos"})
+    }
+    const usuarioARevisar = usuarios.find(usuario => usuario.user === user);
+    if(usuarioARevisar){
+        return res.status(400).send({status:"Error",message:"existencia de usuario confirmada"})
+    }
+    const salt = await bcryptjs.genSalt(5)
+    const hashPassword = await bcryptjs.hash(password,salt)
+    const nuevoUsuario ={
+        user, email, password: hashPassword
+    }
+    usuarios.push(nuevoUsuariosuarios);
+    console.log(usuario);
+    res.status(201).send({status:"ok",message:`Usuario ${nuevoUsuario.user} agregado`,redirect:"/"})
 }
+
+
+
 
 export const methods = {
     login,
     register
-}
+  }
--- app/index.js
+++ app/index.js
@@ -8,17 +8,18 @@ import { methods as authentication } from "./controllers/authentication.controll
 
 //server 
 const app = express();
-app.set("port", 3000);
+app.set("port", 4000);
 app.listen(app.get("port"));
-console.log("server activo on puerto",app.get("port"))
+console.log("server activo on puerto",app.get("port"));
 
 //config
-app.use(express.static(__dirname + "/publicazo"))
-
+app.use(express.static(__dirname + "/publicazo"));
+app.use(express.json());
 
 //routes 
-app.get("/",(req,res)=> res.sendFile(__dirname + "/paginitas/login.html"))
-app.get("/register",(req,res)=> res.sendFile(__dirname + "/paginitas/register.html"))
-app.get("/admin",(req,res)=> res.sendFile(__dirname + "/paginitas/admin/admin.html"))
-app.get("/api/register",authentication.register)
-app.get("/api/login",authentication.login)
+app.get("/",(req,res)=> res.sendFile(__dirname + "/paginitas/login.html"));
+app.get("/register",(req,res)=> res.sendFile(__dirname + "/paginitas/register.html"));
+app.get("/admin",(req,res)=> res.sendFile(__dirname + "/paginitas/admin/admin.html"));
+app.post("/api/register",authentication.register);
+app.post("/api/login",authentication.login);
+  
