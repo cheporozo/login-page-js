ab4e254f20341412454bd6c846f679165e551978
cheporozo <da132413@gmail.com>
2024-02-01 14:08:01 -1100

    Tercer commit

---
+++ app/controllers/authentication.controller.js
@@ -0,0 +1,11 @@
+async function login(req,res){
+}
+
+async function register(req,res){
+    console.log(req,body)
+}
+
+export const methods = {
+    login,
+    register
+}
--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import express from "express"
 import path from 'path';
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+import { methods as authentication } from "./controllers/authentication.controller.js";
 
 
 //server 
@@ -19,4 +19,6 @@ app.use(express.static(__dirname + "/publicazo"))
 //routes 
 app.get("/",(req,res)=> res.sendFile(__dirname + "/paginitas/login.html"))
 app.get("/register",(req,res)=> res.sendFile(__dirname + "/paginitas/register.html"))
-
+app.get("/admin",(req,res)=> res.sendFile(__dirname + "/paginitas/admin/admin.html"))
+app.get("/api/register",authentication.register)
+app.get("/api/login",authentication.login)
